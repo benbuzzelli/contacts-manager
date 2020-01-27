39db261f46baca8901724f85d9cd25b38dbcd4f4
arijdavis <46549985+arijdavis@users.noreply.github.com>
2020-01-27 09:46:02 -0500

    Update view-contacts.component.ts

--- src/app/view-contacts/view-contacts.component.ts
+++ src/app/view-contacts/view-contacts.component.ts
@@ -4,14 +4,27 @@ import { ContactsService } from '../create-contacts/contacts.service';
 import { Observable } from 'rxjs';
 import { Router } from  "@angular/router";
 import { Contact } from '../create-contacts/contacts.service';
+import {DataSource} from '@angular/cdk/collections';
+import {MatTableModule} from '@angular/material/table';
 
+
+/**
+ * Setup files
+ */
 @Component({
   selector: 'app-view-contacts',
   templateUrl: './view-contacts.component.html',
   styleUrls: ['./view-contacts.component.css']
 })
+
+/**
+ * Components to use for viewing contacts table, buttons, and search
+ */
 export class ViewContactsComponent implements OnInit {
   contacts$: Observable<any[]> = null;
+  
+  dataSource = this.contacts$;
+  displayedColumns: string[] = ['name'];
 
   constructor(private contactsService: ContactsService, 
     private db: AngularFireDatabase, public router: Router) { }
